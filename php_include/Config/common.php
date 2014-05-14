5d2f8c3209c1ca49e87223fca65fac66baff3faf
oubakiou <oubakiou@gmail.com>
2014-05-14 10:56:25 +0900

    設定ファイルへのキー追加

--- php_include/Config/common.php
+++ php_include/Config/common.php
@@ -1,25 +1,40 @@
 <?php
 
+$baseDir = realpath(dirname(__FILE__).'/../../');
+
 return [
     'debug'     => false,
     'salt'      => 'qi0KbV^y=)j&$T;(VytxFT2kCU%0~rQ4',
     'timeZone'  => 'Asia/Tokyo',
-    'baseDir'   => realpath(dirname(__FILE__).'/../../'),
-    'publicDir' => realpath(dirname(__FILE__).'/../../') . '/public',
-    'logDir' => realpath(dirname(__FILE__).'/../../') . '/php_include/Var/Log',
+    'baseDir'   => $baseDir,
+    'publicDir' => $baseDir . '/public',
+    'logDir' => $baseDir . '/php_include/Var/Log',
     'internalCharset'     => 'UTF-8',
     'template'=>[
+        'enabledGzip'         => true,
         'outputCharset'       => 'UTF-8',
         'templateCharset'     => 'UTF-8',
-        'templateDir'         => realpath(dirname(__FILE__).'/../../') . '/php_include/Template/Default',
-        'templateCacheDir'    => realpath(dirname(__FILE__).'/../../') . '/Var/Cache/Template/Default',
+        'templateDir'         => $baseDir . '/php_include/Template/Default',
+        'templateCacheDir'    => $baseDir . '/Var/Cache/Template/Default',
         'templateCacheDisable'=> true,
     ],
     'templateFp'=>false,
     'templateSp'=>false,
-    'image'=>[
-        'uploadImgMaxSize'    => 1048576,
-        'uploadImgPath'       => realpath(dirname(__FILE__).'/../../') . '/public/img',
+    'compressor'=>[
+        'enabledGzip'=>true,
+        'cacheDisable'=>true,
+        'js'=>[
+            'cacheDir'=>$baseDir . '/public/js',
+        ],
+        'css'=>[
+            'cacheDir'=>$baseDir . '/public/css',
+        ],
     ],
-    'routingRules'=>[],
+    'image'=>[
+        'uploadImgMaxSize' => 10000000,
+        'uploadImgUrl'     => '/img/up/',
+        'uploadImgPath'    => $baseDir . '/public/img/up/',
+    ],
+    'routingRules'=>[
+    ],
 ];
